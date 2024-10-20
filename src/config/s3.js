cca945f87ccb68a8311f2a770b1d41f595cb5df0
Muhammad Sheharyar <sheharyarone@gmail.com>
2024-10-20 05:16:19 +0500

    var change

--- src/config/s3.js
+++ src/config/s3.js
@@ -5,7 +5,7 @@ const s3 = new S3({
   region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECREet_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECREt_ACCESS_KEY,
   },
 });
 
