70f768ac8cd541b931f32c85ed0c3404605ab31f
Muhammad Sheharyar <sheharyarone@gmail.com>
2024-10-20 05:11:06 +0500

    remove

--- src/models/User.js
+++
@@ -1,9 +0,0 @@
-// src/models/User.js
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  phoneNumber: { type: String, required: true, unique: true }, // Phone number as the primary identifier
-  createdAt: { type: Date, default: Date.now }, // Timestamp field
-}, { timestamps: true });
-
-module.exports = mongoose.model('Auth', userSchema, 'auth');
--- src/routes/sessionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createVideoCvSession } = require('../controllers/sessionController');
-
-// Route for creating a new video CV session
-router.get('/video_cv_session', createVideoCvSession);
-
-module.exports = router;
